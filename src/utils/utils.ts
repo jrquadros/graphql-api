e97bc09cd937aed953a7a64ec3fcb690a17fa5ba
jrquadros <quadrosjunioradits@gmail.com>
2019-05-04 15:41:45 -0300

    JWT ENV not working

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -43,4 +43,4 @@ export const throwError = (condition: boolean, message: string): void => {
     if (condition) { throw new Error(message); }
 };
 
-export const JWT_SECRET: string = process.env.JWT_SECRET;
+export const JWT_SECRET: string = 'iron_man'/*process.env.JWT_SECRET*/;
